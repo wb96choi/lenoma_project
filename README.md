8dcc34ced53f0d56eb2982f002aea9f94c8805e0
1_b1n <choi44601280@gmail.com>
2022-08-02 15:05:24 +0900

    수정

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-## DEMO
+
+
+# 레모나 반응형 웹 홈페이지
+
+## 개요
+    여기에 작품에 대한 소개를 작성
+
+## 작업 내용
+1. 시안 및 프로토타입 제작(figma)
+2. 헤더 toggle gnb 반응형으로 구현
+3. main visual 영역 애니메이션(aos)
+4. media query 와 flex layout 활용
+5. 동영상 모달창 구현
+6. smooth page scroll
+    - scroll-behavior: smooth
+
+## 라이브러리
+- 제이쿼리
+- AOS
+- Bootstrap 아이콘
+
+# 대표사진
+
+# 홈페이지 링크
 https://wb96choi.github.io/lenoma_project/
--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function(){
 
+    // 토글메뉴
 	$("#toggle-btn").click(function(){
 		$("#main-header nav").toggleClass("on");
 	});
@@ -7,4 +8,17 @@ $(function(){
 	// AOS 초기화
 	AOS.init();
 
+    // 모달창
+    $('.modal .box .btn-close').click(function(){
+        $(".modal").removeClass('on');
+    })
+    $('#main-section2 .container .youtube').click(function(){
+        $('.modal').addClass('on')
+    })
 }); // $
+
+// 이미지 및 콘텐츠 로딩 체크
+window.onload = function(){
+    // AOS 초기화
+    AOS.init();
+};
